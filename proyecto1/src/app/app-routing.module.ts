c677255ad439334976538fc830b5a271c815d531
JMALEX9 <jan.masapanta@yavirac.edu.ec>
2022-11-27 20:18:41 -0500

    Subir aqui

--- proyecto1/src/app/app-routing.module.ts
+++ proyecto1/src/app/app-routing.module.ts
@@ -1,39 +1,23 @@
+import { AuthRoutingModule } from './auth/auth-routing.module';
 import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { CatalogueComponent } from './pages/catalogue/catalogue.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProductComponent } from './pages/product/product.component';
-//import { CommonModule } from '@angular/common';
+import { PagesRoutingModule } from './pages/pages-routing.module';
 
-const routes:Routes = [
-  //rutas protegidas 
-  {path : '' ,
-   component:PagesComponent,
-  children:[
-    {path : 'dashboard' , component:DashboardComponent},
-    {path : 'catalogue' , component:CatalogueComponent},
-    {path : 'product' , component:ProductComponent},
-    {path : '' , redirectTo : '/dashboard', pathMatch : 'full'},
-  ]},
+import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundRoutingModule } from './not-found/not-found-routing.module';
 
-  {path : 'login' , component:LoginComponent},
-  {path : 'register' , component:RegisterComponent},
+const routes: Routes = [
+  //* Rutas hijas o rutas protegidas
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
-
-  {path : '**' , component:NotFoundComponent},
-
-
-]
 @NgModule({
   declarations: [],
-  imports: [
-      RouterModule.forRoot(routes) , //especifica las rutas principales
-  ],
-  exports:[RouterModule]
 
+  imports: [CommonModule, RouterModule, PagesRoutingModule,AuthRoutingModule,NotFoundRoutingModule,RouterModule.forRoot(routes)], //*especificamos las rutas principales con forRoot
+
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
